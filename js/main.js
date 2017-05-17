28509cebf9fb7a64da3554b79662ef076315b9cb
Liam Ellis <liamhellis@gmail.com>
2017-05-17 01:58:20 -0400

    contact page

--- js/main.js
+++ js/main.js
@@ -16,6 +16,14 @@ jQuery(document).ready(function($){
 		}
 	});
 
+	$( '.js-input' ).keyup(function() {
+		if( $(this).val() ) {
+	    	$(this).addClass('not-empty');
+	  	} else {
+	    	$(this).removeClass('not-empty');
+	  	}
+	});
+
 });
 
 
